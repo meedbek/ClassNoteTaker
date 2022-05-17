27fd6743c9b67819e54ce3a9fb1cce612a5c6959
meedbek <c_mohammed@hotmail.fr>
2022-05-17 12:22:16 +0100

    add reset password with mailer

--- symfony_project/config/bundles.php
+++ symfony_project/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
