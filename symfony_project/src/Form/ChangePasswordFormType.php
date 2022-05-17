bb64bf9f388d33409e9916376ea8efad855bdf8c
BEN SALAH Abderrahman <40049722+abderrahman-bns@users.noreply.github.com>
2022-05-17 14:43:32 +0200

    final version

--- symfony_project/src/Form/ChangePasswordFormType.php
+++ symfony_project/src/Form/ChangePasswordFormType.php
@@ -30,11 +30,11 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'New password : ',
                 ],
                 'second_options' => [
                     'attr' => ['autocomplete' => 'new-password'],
-                    'label' => 'Repeat Password',
+                    'label' => 'Repeat Password : ',
                 ],
                 'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
